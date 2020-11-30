1b65a20df68498b91b8e9d5eb643fdf945477380
Aseals1 <59372412+Alseals1@users.noreply.github.com>
2020-11-30 12:05:34 -0600

    Add files via upload
    initial commit

---
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // IB Outlet allows me to reference a UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    var leftDiceNumber = 1
+    var rightDiceNumber = 1
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Using dot notation to change the imageView. Who.What = Value
+        
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+       
+               diceImageView1.image = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
+        leftDiceNumber += 1
+        
+        diceImageView2.image = [ #imageLiteral(resourceName: "DiceSix"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceOne")][rightDiceNumber]
+        rightDiceNumber += 1
+    }
+    
+}
+
